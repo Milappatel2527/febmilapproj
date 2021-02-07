1522b419fbb9f979c7e09ca7cfd492bcff9f9603
milappatel2527 <milu_patel1139@yahoo.com>
2021-02-06 22:42:04 -0600

    Milap homework project

---
+++ febmilapproj/src/main/java/com/mycompany/febmilapproj/Febclass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.febmilapproj;
+
+/**
+ *
+ * @author milu_
+ */
+public class Febclass {
+    public static void main (String args []) {
+        int lenght=75;
+        int width=95;
+        int area=lenght*width;
+        System.out.println("area of ractangle="+area);
+                
+               
+    }
+}
